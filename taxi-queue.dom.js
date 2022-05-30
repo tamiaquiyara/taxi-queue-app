1149785ea5916872f2f647d014cf0fe60ac4e510
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-05-30 13:52:18 +0200

    Added element references and eventListeners

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,28 @@
 
 
 // DOM element references
+const join = document.querySelector(".join_queue");
+const leave = document.querySelector(".leave_queue");
+const taxiJoin = document.querySelector(".join_taxi_queue");
+const taxiLeave = document.querySelector(".depart");
 
+const passengerCount = document.querySelector(".passenger_queue_count");
+const taxiCount = document.querySelector(".taxi_queue_count")
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
 
 // DOM events
+join.addEventListener("click", function(){
+    passengerCount.innerHTML = taxiQueue(join.value);
+});
+
+leave.addEventListener("click", function(){
+    passengerCount.innerHTML = taxiQueue(leave.value);
+});
+
+taxiJoin.addEventListener("click", function(){
+    taxiCount.innerHTML = taxiQueue(taxiJoin.value);
+});
+
 
