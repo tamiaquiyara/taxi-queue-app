1961e0acd064aebae532289ab8efb47110e72588
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-05-30 14:29:16 +0200

    added another eventListener

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -15,15 +15,22 @@ const taxiQueue = TaxiQueue();
 
 // DOM events
 join.addEventListener("click", function(){
+    alert(join);
     passengerCount.innerHTML = taxiQueue(join.value);
 });
 
 leave.addEventListener("click", function(){
+    alert(leave);
     passengerCount.innerHTML = taxiQueue(leave.value);
 });
 
 taxiJoin.addEventListener("click", function(){
+    alert(taxiJoin);
     taxiCount.innerHTML = taxiQueue(taxiJoin.value);
 });
 
+taxiLeave.addEventListener("click", function(){
+    alert(taxiLeave);
+    
+})
 
